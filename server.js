5481d91ede921147898b3a87ad95e018adb3cbbe
Swapnil Baburao Diwate <117917508+diwates2232@users.noreply.github.com>
2025-04-12 16:28:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+const cors = require('cors');
+const alarmRoutes = require('./routes/alarmRoutes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use('/api/alarms', alarmRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
