16e96bacfd9becf7eb772144aa1e616ee9a37546
Swapnil Baburao Diwate <117917508+diwates2232@users.noreply.github.com>
2025-04-12 16:28:31 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+const { getAlarmSummary, getRawAlarms } = require('../controllers/alarmController');
+
+router.get('/summary', getAlarmSummary);
+router.get('/raw', getRawAlarms);
+
+module.exports = router;
