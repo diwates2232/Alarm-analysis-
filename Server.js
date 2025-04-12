d3f0855ef85e06092bd05a6e9d05a6dee2db722e
Swapnil Baburao Diwate <117917508+diwates2232@users.noreply.github.com>
2025-04-12 16:27:35 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,13 @@
+const xlsx = require('xlsx');
+const path = require('path');
 
+function loadExcelData() {
+    const filePath = path.join(__dirname, '../data/Alarms.xlsx');
+    const workbook = xlsx.readFile(filePath);
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    return xlsx.utils.sheet_to_json(sheet);
+}
+
+module.exports = {
+    loadExcelData
+};
