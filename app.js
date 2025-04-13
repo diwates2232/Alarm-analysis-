988d412c21cdd072c5093590f408c667b497efa9
Swapnil Baburao Diwate <117917508+diwates2232@users.noreply.github.com>
2025-04-13 13:29:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,27 +29,3 @@ export default App;
 
 
 
-
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import RawAlarms from './pages/RawAlarms';
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <Link to="/">Dashboard</Link> | <Link to="/alarms">Raw Alarms</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/alarms" element={<RawAlarms />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
