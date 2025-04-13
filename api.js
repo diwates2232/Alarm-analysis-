231b2670b8ff4ca8eef5e6289b0075bad50062fb
Swapnil Baburao Diwate <117917508+diwates2232@users.noreply.github.com>
2025-04-13 11:28:50 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,4 @@
+import axios from 'axios';
 
+export const getAlarmSummary = () => axios.get('http://localhost:3000/api/alarms/summary');
+export const getRawAlarms = () => axios.get('http://localhost:3000/api/alarms/raw');
