9e8e25bdf082fe6e7e9c098b3cf03036cf6cbe43
Swapnil Baburao Diwate <117917508+diwates2232@users.noreply.github.com>
2025-04-13 11:29:58 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,1 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import SummaryCards from '../components/SummaryCards';
+import { getAlarmSummary } from '../services/api';
 
+const Dashboard = () => {
+  const [summary, setSummary] = useState(null);
+
+  useEffect(() => {
+    getAlarmSummary()
+      .then(res => setSummary(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  if (!summary) return <div>Loading Summary...</div>;
+
+  return (
+    <div>
+      <SummaryCards summary={summary} />
+    </div>
+  );
+};
+
+export default Dashboard;
