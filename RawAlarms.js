67fd78ce0d6d0866d0f28bcf9e21b69b46c53847
Swapnil Baburao Diwate <117917508+diwates2232@users.noreply.github.com>
2025-04-13 11:31:32 +0530

    Update RawAlarms.js

--- RawAlarms.js
+++ RawAlarms.js
@@ -1,1 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import AlarmsTable from '../components/AlarmsTable';
+import { getRawAlarms } from '../services/api';
 
+const RawAlarms = () => {
+  const [alarms, setAlarms] = useState([]);
+
+  useEffect(() => {
+    getRawAlarms()
+      .then(res => setAlarms(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <AlarmsTable data={alarms} />
+    </div>
+  );
+};
+
+export default RawAlarms;
