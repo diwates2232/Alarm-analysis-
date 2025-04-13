2bc0f95af968767f92205539d7e4ea0a265f160a
Swapnil Baburao Diwate <117917508+diwates2232@users.noreply.github.com>
2025-04-13 11:26:31 +0530

    Update AlarmTable.js

--- AlarmTable.js
+++ AlarmTable.js
@@ -1,1 +1,41 @@
+import React from 'react';
 
+const AlarmsTable = ({ data }) => {
+  return (
+    <div>
+      <h3>Raw Alarm Logs</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Location</th>
+            <th>Region</th>
+            <th>Rejection</th>
+            <th>Priority</th>
+            <th>Operator</th>
+            <th>Action Taken</th>
+            <th>SLA</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((alarm, index) => (
+            <tr key={index}>
+              <td>{alarm.Date}</td>
+              <td>{alarm["Time of  Alarm (Local time)"]}</td>
+              <td>{alarm.Location}</td>
+              <td>{alarm.Region}</td>
+              <td>{alarm.Rejection}</td>
+              <td>{alarm["CCURE Incident Priority"]}</td>
+              <td>{alarm["Name of Person Attending Alarms (First, Last Name)"]}</td>
+              <td>{alarm["Action Taken"]}</td>
+              <td>{alarm["Target SLA in Mins"]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AlarmsTable;
